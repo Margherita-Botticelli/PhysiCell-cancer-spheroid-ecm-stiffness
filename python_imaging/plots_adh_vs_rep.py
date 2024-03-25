489968bbd8f5f5d6bff6af48b24367b20b61775a
Margherita-Botticelli <mxb1267@student.bham.ac.uk>
2024-03-25 14:25:51 +0000

    Update plots_adh_vs_rep.py

--- python_imaging/plots_adh_vs_rep.py
+++ python_imaging/plots_adh_vs_rep.py
@@ -13,7 +13,7 @@ def plots_adh_vs_rep(data, simulation_name, save_folder):
 
     ribose = data['ribose'].iloc[0]
     t = np.unique(data['t']).astype(float)
-    prolif = data['prolif'].iloc[0]
+    prolif = round(float(data['prolif'].iloc[0]),5)
     max_mot_speed = data['max_mot_speed'].iloc[0]
 
     spheroid_area_ratio_mean = []
@@ -42,26 +42,31 @@ def plots_adh_vs_rep(data, simulation_name, save_folder):
         cell_adh.append(float(df_sim['cell_adh'].iloc[0]))
         cell_rep.append(float(df_sim['cell_rep'].iloc[0]))
     
-    size_rep = len(np.unique(cell_rep))
-    size_adh = len(np.unique(cell_adh))
+    # size_adh = len(np.unique(cell_adh))
+    # size_rep = len(np.unique(cell_rep))
 
     columns = np.unique(cell_adh) 
     index = np.flip(np.unique(cell_rep))
 
-    # annot_arr = 
-
     df = pd.DataFrame(columns=columns, index=index)
     df = df.fillna(0.0)
+
+    annot_df = pd.DataFrame(columns=columns, index=index)
+    annot_df = annot_df.fillna('NaN')
+    print(annot_df,flush=True)
+    for adh,rep,area_mean,area_std in zip(cell_adh,cell_rep,spheroid_area_ratio_mean,spheroid_area_ratio_std):
+        df[adh][rep] = area_mean
+
+        annot_mean = "%.2f" % area_mean
+        annot_std = "%.2f" % area_std
+        annot = annot_mean + '\n' + u'\u00B1' + annot_std
+        annot_df[adh][rep] = annot
     
-    for adh,rep,area in zip(cell_adh,cell_rep,spheroid_area_ratio_mean):
-        df[adh][rep] = area
+    print(annot_df,flush=True)
 
-        # annot_mean = ["%.2f" % number for number in spheroid_area_ratio_mean]
-        # annot_std = ["%.2f" % number for number in spheroid_area_ratio_std]
-        # annot_pm = [u'\u00B1']*len(annot_mean)
-        # annot_arr = np.array([a+b+c for a,b,c in zip(annot_mean,annot_pm,annot_std)])
-        # annot_arr = np.reshape(annot_arr, (size,-1))
+    annot_arr = annot_df.to_numpy()
 
+    print(annot_arr,flush=True)
 
     # if len(np.unique(cell_adh))<len(np.unique(cell_rep)):
     #     size = len(np.unique(cell_adh))
@@ -98,11 +103,11 @@ def plots_adh_vs_rep(data, simulation_name, save_folder):
 
     cmap = mpl.colors.LinearSegmentedColormap.from_list("", ['white',color_rib])
 
-    print(df,flush=True)
+    # print(df,flush=True)
 
 
-    # ax = seaborn.heatmap(df,cmap=cmap,vmin=1, vmax=7,annot = annot_arr, fmt="s", cbar_kws={'label': 'Growth'})
-    ax = seaborn.heatmap(df,cmap=cmap,vmin=1, vmax=7, cbar_kws={'label': 'Growth'})
+    ax = seaborn.heatmap(df,cmap=cmap,vmin=1, vmax=7,annot = annot_arr, fmt="s", cbar_kws={'label': 'Growth'})
+    # ax = seaborn.heatmap(df,cmap=cmap,vmin=1, vmax=7, cbar_kws={'label': 'Growth'})
 
     ax.figure.axes[-1].yaxis.set_label_position('left')
 
