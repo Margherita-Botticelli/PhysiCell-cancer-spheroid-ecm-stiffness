71d54737563572283d7d5ded3027b630cb7f99b5
Margherita-Botticelli <mxb1267@student.bham.ac.uk>
2024-05-14 12:42:37 +0100

    Update python_imaging_plots.sh

--- python_imaging_plots.sh
+++ python_imaging_plots.sh
@@ -2,6 +2,7 @@
 #SBATCH --qos bbdefault
 #SBATCH --ntasks 10
 #SBATCH --mem-per-cpu=8G
+#SBATCH --nodes 1
 #SBATCH --time 3000:0
 #SBATCH --account spillf-systems-mechanobiology-health-disease
 
@@ -11,6 +12,7 @@ module purge
 module load bluebear
 module load Miniconda3/4.9.2
 module load bear-apps/2022a
+# module load FFmpeg
 
 source activate python_imaging
 
