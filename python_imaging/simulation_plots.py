a6e082ff68bed5da103cbb1939aa657b27701f0f
Margherita-Botticelli <mxb1267@student.bham.ac.uk>
2024-10-09 10:18:21 +0100

    Update simulation_plots.py

--- python_imaging/simulation_plots.py
+++ python_imaging/simulation_plots.py
@@ -6,6 +6,7 @@ from joblib import Parallel, delayed # type: ignore
 import pandas as pd
 from simulation_data import *
 from plots_over_time import *
+from box_plots import *
 from heatmaps import *
 from spheroid_area_function import spheroid_area_function
 from delaunay_function import *
@@ -35,13 +36,13 @@ if __name__ == '__main__':
     data_folder = f'../data/{proj}/'
 
     #### Define simulations to run
-    simulations = list(range(0,288)) # list(range(0,96)) # Example: [240], [291], list(range(0,240)), etc.
+    simulations = list(range(0,240)) # [240,293,420] # [293,420] # list(range(320,420)) # list(range(0,240)) # list(range(240,320)) # Example: [0], list(range(0,240)), etc.
   
     #### Flag to determine if existing data should be replaced
-    replacing = True
+    replacing = False
     
     #### List of ribose concentrations to test
-    riboses = [0]  # Options: [0, 50, 200]
+    riboses = [0] #,50,200]  # Options: [0], [50], [200]
 
     #### Number of random seeds for simulations
     n_seeds = 10
@@ -49,13 +50,14 @@ if __name__ == '__main__':
 
     #### Flags for different types of plots
     #### Title on plots
-    title = True
-    plots_over_time = True
+    title = False
+    plots_over_time = False 
+    box_plots = False
     heatmaps_speed_vs_degr = True
     heatmaps_sigma_vs_delta = False
-    heatmap_time_points = [96*60]  # Example: [48*60, 96*60]
-    time_point_images = True
-    times = [96*60] # Time points to consider for images, Options: 'all', [24*60, 48*60, 72*60, 96*60]
+    heatmap_time_points = [96*60]  # Example: [48*60], [96*60]
+    time_point_images = False
+    times = [24*60, 96*60] # Time points to consider for images, Options: 'all', [24*60, 48*60, 72*60, 96*60], 
     video = False
     statistics = False
 
@@ -102,6 +104,23 @@ if __name__ == '__main__':
                 plots_delaunay_mean_distance_over_time(data, save_folder, title=title)
                 plots_cell_count_over_time(data, save_folder, title=title)
             plt.close('all')
+
+        print('plots over time done!', flush=True)
+
+
+    ########## BOX PLOTS ###########
+    if box_plots:
+        # box_plot_spheroid_area_growth(df, save_folder, title=title)
+        # plt.close('all')
+
+        for sim in simulations:
+            data = df[(df['simulation'] == sim)]
+            # box_plot_spheroid_area_growth(data, save_folder, title=title)
+            box_plot_delaunay_mean_distance(data, save_folder, title=title)
+            box_plot_cell_count(data, save_folder, title=title)
+        plt.close('all')
+
+        print('Box plots done!', flush=True)
 
 
     ######## HEATMAP PLOTS #########
@@ -167,40 +186,43 @@ if __name__ == '__main__':
 
     ######### TIME POINT IMAGE ##############
     #### Check if time point images should be generated
-    if time_point_images:
+    if(time_point_images or video):
         for sim in simulations:
             for rib in riboses:
-                if times == 'all':
-                    times = np.unique(df[(df['seed'] == 0) & (df['ID'] == 0)]['t']).astype(int)
-                for t in times:
+                if time_point_images:
+                    if time_point_images == 'all':
+                        times = np.unique(df[(df['seed'] == 0) & (df['ID'] == 0)]['t']).astype(int)
+                    for t in times:
 
-                    #### Filter data
-                    seed = 0
-                    data = df[(df['simulation'] == sim) & (df['ribose'] == rib) & (df['seed'] == seed) & (df['t'] == t)]
+                        #### Filter data
+                        seed = 0
+                        data = df[(df['simulation'] == sim) & (df['ribose'] == rib) & (df['seed'] == seed) & (df['t'] == t)]
 
-                    #### Get time point to find snapshot
-                    time_step = data[data['ID'] == 0].index.values.astype(int)[0]
-                    snapshot = 'output' + '{:08d}'.format(int(time_step))
-                    data_folder_sim = data_folder + f'output_rib{rib}_{sim}_{seed}/'
-                    save_name = save_folder + f'images/full_image_rib{rib}_{sim}_{seed}_t{int(t):04}.png'
+                        #### Get time point to find snapshot
+                        time_step = data[data['ID'] == 0].index.values.astype(int)[0]
+                        snapshot = 'output' + '{:08d}'.format(int(time_step))
+                        data_folder_sim = data_folder + f'output_rib{rib}_{sim}_{seed}/'
+                        save_name = save_folder + f'images/full_image_rib{rib}_{sim}_{seed}_t{int(t):04}.png'
 
-                    #### Generate images
-                    create_plot(data, snapshot, data_folder_sim, save_name, output_plot=True, title=title) 
+                        #### Generate images
+                        create_plot(data, snapshot, data_folder_sim, save_name, output_plot=True, title=title) 
                 
-            #### Check if video should be generated
-            if video:
-                seed = 0
-                video_name = save_folder + f'animations/video_rib{rib}_{sim}_{seed}.mp4'
+                #### Check if video should be generated
+                if video:
+                    seed = 0
+                    video_name = save_folder + f'animations/video_rib{rib}_{sim}_{seed}.mp4'
 
-                #### Find generated images
-                images = save_folder + f'images/full_image_rib{rib}_{sim}_{seed}_t*.png'
+                    #### Find generated images
+                    images = save_folder + f'images/full_image_rib{rib}_{sim}_{seed}_t*.png'
 
-                #### Generate video
-                os.system(f'ffmpeg -y -framerate 10 -pattern_type glob -i \'{images}\' -c:v libx264 -pix_fmt yuv420p -vf pad="width=ceil(iw/2)*2:height=ceil(ih/2)*2" {video_name}')
+                    #### Generate video
+                    os.system(f'ffmpeg -y -framerate 10 -pattern_type glob -i \'{images}\' -c:v libx264 -pix_fmt yuv420p -vf pad="width=ceil(iw/2)*2:height=ceil(ih/2)*2" {video_name}')
+
+                    print('Video ready!', flush=True)
 
     ######### STATISTICS IMAGES ##########
     if statistics:
-        data = df[df['t'] == 96*60]
-        print(data, flush=True)
-        delaunay_distance_function(data, save_folder=save_folder, figure=True)
-        spheroid_area_function(data, save_folder=save_folder, figure=True)
+        for sim in simulations:
+            data = df[(df['simulation'] == sim) & (df['seed'] == 0) & (df['t'] == 96*60)]
+            delaunay_distance_function(data, save_folder=save_folder, figure=True)
+            spheroid_area_function(data, save_folder=save_folder, figure=True)
