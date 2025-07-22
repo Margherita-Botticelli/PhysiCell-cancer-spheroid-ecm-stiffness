1068195c50e625e86ce6b7adbf107c475e73de82
Margherita-Botticelli <mxb1267@student.bham.ac.uk>
2025-07-22 12:10:31 +0100

    Update simulation_plots.py

--- python_imaging/simulation_plots.py
+++ python_imaging/simulation_plots.py
@@ -15,7 +15,6 @@ from skimage import io
 from PIL import Image
 import os
 import warnings
-from least_squares_function import *
 
 #### Suppress future warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -26,16 +25,18 @@ if __name__ == '__main__':
     mpl.rcParams['figure.dpi'] = 600
     
     #### Set plotting style for consistency
+    plt.style.use('ggplot')
     plt.style.use('seaborn-v0_8-colorblind')
 
     #### Specify the project to work on
-    proj = 'ecm_density'  # Options: 'tests', 'ecm_fibers'
+    proj = 'ecm_density'  # Options: 'tests', 'ecm_fibres'
     
     #### Define folders for saving results and loading data
     save_folder = f'../results/{proj}/'
     data_folder = f'../data/{proj}/'
 
     #### Define simulations to run
+<<<<<<< HEAD
     
     #### Non-invasive simulations
     # simulations = [240] # Set riboses = [0,50,200]
@@ -68,12 +69,20 @@ if __name__ == '__main__':
     # simulations = list(range(0,3))
     # simulations = [240,293]
 
+=======
+    simulations = list(range(0,240)) # [240,293,420] # [293,420] # list(range(320,420)) # list(range(0,240)) # list(range(240,320)) # Example: [0], list(range(0,240)), etc.
+  
+>>>>>>> parent of 8723b27 (update python_imaging)
     #### Flag to determine if existing data should be replaced
     replacing = False
     
     #### List of ribose concentrations to test
+<<<<<<< HEAD
     riboses = [0] #[50,200] # [50,200] # Options: 0, 50, 200
 >>>>>>> parent of 7c561b3 (Update simulation_plots.py)
+=======
+    riboses = [0] #,50,200]  # Options: [0], [50], [200]
+>>>>>>> parent of 8723b27 (update python_imaging)
 
     #### Number of random seeds for simulations
     n_seeds = 1
@@ -81,21 +90,25 @@ if __name__ == '__main__':
 
     #### Flags for different types of plots
 <<<<<<< HEAD
+<<<<<<< HEAD
     title = True # Title on plots
     least_squares = False
     plots_over_time = True 
 =======
     title = False # Title on plots
     least_squares = True
+=======
+    #### Title on plots
+    title = False
+>>>>>>> parent of 8723b27 (update python_imaging)
     plots_over_time = False 
 >>>>>>> parent of 7c561b3 (Update simulation_plots.py)
     box_plots = False
-    contour = False # Contour lines on heatmaps
-    heatmaps_speed_vs_degr = False
+    heatmaps_speed_vs_degr = True
     heatmaps_sigma_vs_delta = False
-    heatmap_time_points = [72*60]  # Example: [48*60], [96*60]
-    time_point_images = False # 'all' # True # Options: 'all' if you want all time points
-    times = [72*60] #[96*60] # Time points to consider for images, Options: [24*60, 48*60, 72*60, 96*60]
+    heatmap_time_points = [96*60]  # Example: [48*60], [96*60]
+    time_point_images = False
+    times = [24*60, 96*60] # Time points to consider for images, Options: 'all', [24*60, 48*60, 72*60, 96*60], 
     video = False
     statistics = False
 
@@ -110,20 +123,12 @@ if __name__ == '__main__':
     data_folder_list = [data_folder] * len(seeds) * len(simulations) * len(riboses)
     replace_list = [replacing] * len(seeds) * len(simulations) * len(riboses)
 
-    # simulations_list = np.repeat([simulations], len(seeds))
-    # seeds_list = seeds * len(simulations) 
-    # data_folder_list = [data_folder] * len(seeds) * len(simulations)
-    # replace_list = [replacing] * len(seeds) * len(simulations) 
-
-    ### Run simulations in parallel
-    # Parallel(n_jobs=10)(delayed(simulation_data)(data_folder, simulation, ribose, seed, replace)
-    #                         for data_folder, simulation, ribose, seed, replace in zip(data_folder_list, simulations_list, riboses_list, seeds_list, replace_list))
-
-    # Parallel(n_jobs=10)(delayed(simulation_data)(data_folder, simulation, seed, replace) 
-    #                         for data_folder, simulation, seed, replace in zip(data_folder_list, simulations_list, seeds_list, replace_list))
+    #### Run simulations in parallel
+    Parallel(n_jobs=10)(delayed(simulation_data)(data_folder, simulation, ribose, seed, replace)
+                            for data_folder, simulation, ribose, seed, replace in zip(data_folder_list, simulations_list, riboses_list, seeds_list, replace_list))
 
     #### Notify when parallel processing is complete
-    # print('Parallel end\n', flush=True)
+    print('Parallel end\n', flush=True)
 
     #### Initialize an empty DataFrame and list to store simulation data
     df = pd.DataFrame()
@@ -134,24 +139,19 @@ if __name__ == '__main__':
         for rib in riboses:
             for seed in seeds:
                 df_new = pd.read_pickle(data_folder + f'output_rib{rib}_{sim}_{seed}/dataframe_rib{rib}_{sim}_{seed}.pkl')
-                # df_new = pd.read_pickle(data_folder + f'output_{sim}_{seed}/dataframe_{sim}_{seed}.pkl')
                 df_list.append(df_new)
 
     #### Concatenate all dataframes into a single dataframe
     df = pd.concat(df_list, copy=False, axis=0)
-
     print('Dataframe ready!\n', flush=True)
-    
-    if least_squares:
-        data = df[df['ID'] == 0]
-        # least_squares_function_approximated(data, save_folder)
-        least_squares_function_cell_count(df, save_folder)
+
 
     ########## PLOTS OVER TIME ###########
     if plots_over_time:
         for sim in simulations:
             for rib in riboses:
                 data = df[(df['simulation'] == sim) & (df['ribose'] == rib)]
+<<<<<<< HEAD
                 print(f'plots over time {sim=}', flush=True)
 
 <<<<<<< HEAD
@@ -166,9 +166,11 @@ if __name__ == '__main__':
                 # data = df[(df['simulation'] == sim)]
                 # plots_spheroid_area_growth_over_time(data, save_folder, title=title)
                 # plots_delaunay_mean_distance_over_time(data, save_folder, title=title)
+=======
+                plots_spheroid_area_growth_over_time(data, save_folder, title=title)
+                plots_delaunay_mean_distance_over_time(data, save_folder, title=title)
+>>>>>>> parent of 8723b27 (update python_imaging)
                 plots_cell_count_over_time(data, save_folder, title=title)
-                # plots_invasion_over_time(data, save_folder, title=title)
-                # plt.close('all')
             plt.close('all')
 >>>>>>> parent of 7c561b3 (Update simulation_plots.py)
 
@@ -177,16 +179,15 @@ if __name__ == '__main__':
 
     ########## BOX PLOTS ###########
     if box_plots:
-        if sim == 420:
-            box_plot_spheroid_area_growth(df, save_folder, title=title)
-            plt.close('all')
-        else:
-            for sim in simulations:
-                data = df[(df['simulation'] == sim)]
-                # box_plot_spheroid_area_growth(data, save_folder, title=title)
-                box_plot_delaunay_mean_distance(data, save_folder, title=title)
-                box_plot_cell_count(data, save_folder, title=title)
-            plt.close('all')
+        # box_plot_spheroid_area_growth(df, save_folder, title=title)
+        # plt.close('all')
+
+        for sim in simulations:
+            data = df[(df['simulation'] == sim)]
+            # box_plot_spheroid_area_growth(data, save_folder, title=title)
+            box_plot_delaunay_mean_distance(data, save_folder, title=title)
+            box_plot_cell_count(data, save_folder, title=title)
+        plt.close('all')
 
         print('Box plots done!', flush=True)
 
@@ -217,7 +218,7 @@ if __name__ == '__main__':
                     simulation_name_plot = simulations_heatmap[0]
                     
                     #### Generate the heatmap for spheroid area growth
-                    plots_speed_vs_degr_spheroid_area_growth(data_spheroid_area_growth, simulation_name_plot, save_folder, title=title, contour=contour)
+                    plots_speed_vs_degr_spheroid_area_growth(data_spheroid_area_growth, simulation_name_plot, save_folder, title=title)
                     print('plots_speed_vs_degr_spheroid_area_growth done!', flush=True)
 
                 #### Filter data to loop through unique proliferation rates
@@ -232,7 +233,7 @@ if __name__ == '__main__':
                     simulation_name_plot = simulations_heatmap[0]
                     
                     #### Generate the heatmap for Delaunay mean distance
-                    plots_speed_vs_degr_delaunay(data_delaunay, simulation_name_plot, save_folder, title=title, contour=contour)
+                    plots_speed_vs_degr_delaunay(data_delaunay, simulation_name_plot, save_folder, title=title)
                     print('plots_speed_vs_degr_delaunay done!', flush=True)
 
             #### Check if heatmaps for sigma vs delta should be generated
@@ -245,7 +246,7 @@ if __name__ == '__main__':
                 simulation_name_plot = simulations_heatmap[0]
                 
                 #### Generate the heatmap for spheroid area growth
-                plots_sigma_vs_delta_spheroid_area_growth(data_spheroid_area_growth, simulation_name_plot, save_folder, title=title, contour=contour)
+                plots_sigma_vs_delta_spheroid_area_growth(data_spheroid_area_growth, simulation_name_plot, save_folder, title=title)
                 print('plots_sigma_vs_delta_spheroid_area_growth done!', flush=True)
 
     #### Close all plot figures to free up memory
@@ -256,42 +257,32 @@ if __name__ == '__main__':
     #### Check if time point images should be generated
     if(time_point_images or video):
         for sim in simulations:
-            for seed in seeds:
-                for rib in riboses:
-                    if time_point_images:
-                        if time_point_images == 'all':
-                            times = np.unique(df[(df['seed'] == seed) & (df['ID'] == 0)]['t']).astype(int)
-                            # print('dataframe', flush=True)
-                        for t in times:
+            for rib in riboses:
+                if time_point_images:
+                    if time_point_images == 'all':
+                        times = np.unique(df[(df['seed'] == 0) & (df['ID'] == 0)]['t']).astype(int)
+                    for t in times:
 
-                            #### Filter data
-                            seed = 0
-                            data = df[(df['simulation'] == sim) & (df['ribose'] == rib) & (df['seed'] == seed) & (df['t'] == t)]
-                            # data = df[(df['simulation'] == sim) & (df['seed'] == seed) & (df['t'] == t)]
+                        #### Filter data
+                        seed = 0
+                        data = df[(df['simulation'] == sim) & (df['ribose'] == rib) & (df['seed'] == seed) & (df['t'] == t)]
 
-                            # print(data, flush=True)
-                            #### Get time point to find snapshot
-                            time_step = data[data['ID'] == 0].index.values.astype(int)[0]
-                            snapshot = 'output' + '{:08d}'.format(int(time_step))
-                            data_folder_sim = data_folder + f'output_rib{rib}_{sim}_{seed}/'
-                            save_name = save_folder + f'images/full_image_rib{rib}_{sim}_{seed}_t{int(t):04}.png'
-                            # data_folder_sim = data_folder + f'output_{sim}_{seed}/'
-                            # save_name = save_folder + f'images/full_image_{sim}_{seed}_t{int(t):04}.png'
+                        #### Get time point to find snapshot
+                        time_step = data[data['ID'] == 0].index.values.astype(int)[0]
+                        snapshot = 'output' + '{:08d}'.format(int(time_step))
+                        data_folder_sim = data_folder + f'output_rib{rib}_{sim}_{seed}/'
+                        save_name = save_folder + f'images/full_image_rib{rib}_{sim}_{seed}_t{int(t):04}.png'
 
-                            print(f'{sim=}, {t=}', flush=True)
-                            #### Generate images
-                            create_plot(data, snapshot, data_folder_sim, save_name, output_plot=True, title=title) 
-                            plt.close('all')
-
+                        #### Generate images
+                        create_plot(data, snapshot, data_folder_sim, save_name, output_plot=True, title=title) 
                 
                 #### Check if video should be generated
                 if video:
-                    # video_name = save_folder + f'animations/video_rib{rib}_{sim}_{seed}.mp4'
-                    video_name = save_folder + f'animations/video_{sim}_{seed}.mp4'
+                    seed = 0
+                    video_name = save_folder + f'animations/video_rib{rib}_{sim}_{seed}.mp4'
 
                     #### Find generated images
-                    # images = save_folder + f'images/full_image_rib{rib}_{sim}_{seed}_t*.png'
-                    images = save_folder + f'images/full_image_{sim}_{seed}_t*.png'
+                    images = save_folder + f'images/full_image_rib{rib}_{sim}_{seed}_t*.png'
 
                     #### Generate video
                     os.system(f'ffmpeg -y -framerate 10 -pattern_type glob -i \'{images}\' -c:v libx264 -pix_fmt yuv420p -vf pad="width=ceil(iw/2)*2:height=ceil(ih/2)*2" {video_name}')
